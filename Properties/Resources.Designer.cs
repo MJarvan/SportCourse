b74578cb2c4d9643723fe5cde953b99e03c4bffc
MJARVAN <mj.630313091@outlook.com>
2017-04-12 11:51:46 +0800

    原来跟资源类代码没有关系，已经优化完毕。

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace sports_course.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
-        public static System.Drawing.Bitmap bw {
-            get {
-                object obj = ResourceManager.GetObject("bw", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
-        /// </summary>
         public static System.Drawing.Bitmap close {
             get {
                 object obj = ResourceManager.GetObject("close", resourceCulture);
